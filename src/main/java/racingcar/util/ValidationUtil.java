f939803ab5258b6dd487c9ca81fb50d1e8ab39ea
janghomoon <sonyjangho@gmail.com>
2022-10-08 16:55:08 +0900

    test : validationUtil return value 오류 수정

--- src/main/java/racingcar/util/ValidationUtil.java
+++ src/main/java/racingcar/util/ValidationUtil.java
@@ -5,15 +5,15 @@ import org.junit.platform.commons.util.StringUtils;
 public class ValidationUtil {
 
   private static final Integer MAX_LENGTH = 5;
-  public static boolean lengthCheck(String input) {
-    if (input.length() <= MAX_LENGTH) {
+  public static boolean isMaxLength(String input) throws IllegalArgumentException {
+    if (input.length() > MAX_LENGTH) {
       return true;
     }
     return false;
   }
 
-  public static boolean isNull(String input) {
-    if(StringUtils.isNotBlank(input)) {
+  public static boolean isNull(String input) throws IllegalArgumentException {
+    if(StringUtils.isBlank(input)) {
       return true;
     }
     return false;
