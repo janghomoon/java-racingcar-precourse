473aeffcb63264d1043cca1e9807c8706c7ec002
janghomoon <sonyjangho@gmail.com>
2022-10-09 20:44:31 +0900

    feat : Message 게임 실행 부터 종료 시 우승자 메세지 추가

--- src/main/java/racingcar/code/Message.java
+++ src/main/java/racingcar/code/Message.java
@@ -3,7 +3,12 @@ package racingcar.code;
 public enum Message {
   //error
   ERROR_INPUT_BLANK("[ERROR] 자동차 이름은 필수입니다."),
-  ERROR_NAME_LENGTH("[ERROR] 자동차 이름은 5자 이하로 입력해야합니다.");
+  ERROR_NAME_LENGTH("[ERROR] 자동차 이름은 5자 이하로 입력해야합니다."),
+  FIRST_MESSAGE("경주할 자동차 이름을 입력하세요.(이름은쉼표(,) 기준으로 구분)"),
+  COLLECT_MESSAGE("시도할 회수는 몇회인가요?"),
+  END_MESSAGE("실행 결과"),
+  WINNER_MESSAGE("최종 우승자: "),
+  ;
 
   private String msg;
   Message(String msg) {
@@ -12,5 +17,9 @@ public enum Message {
 
   public String getMsg() {
     return this.msg;
+  }
+
+  public String getWinner(String winner) {
+    return String.format("%s %s", this.WINNER_MESSAGE.getMsg(), winner);
   }
 }
