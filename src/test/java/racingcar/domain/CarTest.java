e3a7f7dae141ac0de1d82e634735a0a062ec1134
janghomoon <sonyjangho@gmail.com>
2022-10-10 00:00:39 +0900

    test :carTest 도메인 변경

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -2,6 +2,7 @@ package racingcar.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -10,11 +11,15 @@ public class CarTest {
 
   private Car car;
 
+  @BeforeEach
+  void setUp() {
+    car = new Car("test", 0, "");
+  }
+
   @ParameterizedTest
   @DisplayName("4이상 일시 자동차 전진")
   @CsvSource({ "0, false", "3, false", "4, true" })
   void 자동차전진(Integer distance, boolean isMove) {
-    car = new Car("go", 0);
     car.isMove(distance);
     assertThat(car.getDistance() != 0).isEqualTo(isMove);
   }
@@ -23,7 +28,6 @@ public class CarTest {
   @DisplayName("3이하 일시 자동차 멈춤")
   @CsvSource({ "0, true", "3, true", "4, false" })
   void 자동차멈춤(Integer distance, boolean isMove) {
-    car = new Car("stop", 0);
     car.isMove(distance);
     assertThat(car.getDistance() == 0).isEqualTo(isMove);
   }
